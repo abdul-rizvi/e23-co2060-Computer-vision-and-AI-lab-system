0dbb553369e4c9a7794ad09cebfa0f212b2057e5
abdul-rizvi <e23289@eng.pdn.ac.lk>
2026-04-28 09:57:22 +0530

    implement dynamic inventory booking system with QR access passes and backend data seeding

--- code/backend/routes/inventoryRoutes.js
+++ code/backend/routes/inventoryRoutes.js
@@ -1,22 +1,11 @@
 const express = require("express")
 const router = express.Router();
-const multer = require('multer');
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname);
-    }
-});
-const upload = multer({ storage });
 
-const { getAllItems, createItem, deleteItem, importEquipmentData } = require("../controllers/inventoryController");
+const { getAllItems, createItem, deleteItem } = require("../controllers/inventoryController");
 const { verifyToken, requireRole } = require("../middleware/authMiddleware");
 
 router.get("/", verifyToken, getAllItems);                              // any logged in user
 router.post("/", verifyToken, requireRole("officer"), createItem);     // only officer
 router.delete("/:id", verifyToken, requireRole("officer"), deleteItem); // only officer
-router.post('/import', verifyToken, upload.single('file'), requireRole('admin'), importEquipmentData);   // Excel file upload
 
 module.exports=router;
